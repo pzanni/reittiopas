f6566ffdcae1ebe8561c39cd06dc5becaa0f9b40
Anni <puurunen.anni@gmail.com>
2020-04-29 22:18:55 +0300

    mark place for dijkstra

---
+++ reittiopas/src/backend/services/routeService.js
@@ -0,0 +1,10 @@
+const getFastestRoute = (start, end, lines) => {
+    // Dijkstra comes here
+    // start: string (start point)
+    // end: string (end point)
+    // lines: array (bus lines you can use)
+}
+
+const getLinesForRoute = (route) => {
+  
+}
